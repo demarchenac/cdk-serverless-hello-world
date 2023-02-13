5a5be0b35cbdc68c56c28068e3537c8d9a0baf3c
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2023-02-13 17:48:55 -0500

    Initial commit

---
+++ test/cdk-serverless-hello-world.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkServerlessHelloWorld from '../lib/cdk-serverless-hello-world-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-serverless-hello-world-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkServerlessHelloWorld.CdkServerlessHelloWorldStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
